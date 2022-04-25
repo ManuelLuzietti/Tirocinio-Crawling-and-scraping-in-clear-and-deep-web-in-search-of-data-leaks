4299803bdc53f9ea59af3edf026b9a98f1aa0d5c
ManuelLuzietti <ManuelLuzietti@studio.unibo.it>
2022-04-25 11:49:41 -0400

    adding tor proxy to scraper test 1

--- ScraperTest/provaScraper1.py
+++ ScraperTest/provaScraper1.py
@@ -5,21 +5,29 @@ Created on Sun Apr 24 14:45:10 2022
 @author: manue
 """
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-options = webdriver.ChromeOptions()
-options.add_argument("headless") #disable the browser gui display
-driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
-driver.get("https://www.w3schools.com/html/default.asp")
-content = driver.page_source
-soup = BeautifulSoup(content,features="html.parser") #parsing
-elements = []
-for a in soup.select("div.w3-example > h3"):
-    elements.append(a)
-driver.close()
-print(elements)
+def main(URL):
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from bs4 import BeautifulSoup
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless") #disable the browser gui display
+    options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
+    options.add_argument('user-agent= Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0')
+    driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager().install()))
+    driver.get(URL)
+    #print(driver.page_source)
+    content = driver.page_source
+    soup = BeautifulSoup(content,features="html.parser") #parsing
+    elements = []
+    for a in soup.select("h1"):
+        elements.append(a)
+    driver.close()
+    print(elements)
+    
 
-
-
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        exit()
+    main(sys.argv[1])
--- ScraperTest/provaScraper2-scapy/project/project/spiders/links_spider.py
+++ ScraperTest/provaScraper2-scapy/project/project/spiders/links_spider.py
@@ -5,13 +5,11 @@ class LinksSpider(scrapy.Spider):
     name = "links"
 
     def start_requests(self):
-        urls = ["https://www.w3schools.com/",
-                "https://www.w3schools.com/tags/default.asp"]
+        urls = ["http://c32zjeghcp5tj3kb72pltz56piei66drc63vkhn5yixiyk4cmerrjtid.onion/"]
         for url in urls:
             yield scrapy.Request(url, callback=self.parse)
     
     def parse(self, response):
         for numm,link in enumerate(response.css("a"),start=1):
-            yield {numm:link.get(),
-                    "link": link.css("a").get()}
+            yield {numm:link.get()}
 
