e335727cd23319327ee129f9277d098cf9557e74
ManuelLuzietti <manuel.luzietti@studio.unibo.it>
2022-05-13 13:21:36 -0400

    recursive step enabled

--- ScraperTest/provaScraper3/Scraper.py
+++ ScraperTest/provaScraper3/Scraper.py
@@ -7,21 +7,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import urllib.parse as up
 from collections import deque
-
+import os 
 class Scraper():
     _options = None
     _driver = None
     _currentUrl = None
     _soup = None
-    _urls = []
-    _extracted = []
+    _extracted = set()
     _visitedWebsites = []
     _webstack = deque()
+    _blockedExt = (".pdf")
     
 
-    def __init__(self,headless=True,tor=True,useragent="default"):
+    def __init__(self,headless=True,tor=True,useragent="default",debug=False):
         self._options = webdriver.ChromeOptions()
-        currentScraping = None
+        prefs = {
+            "download_restrictions": 3,
+        }
+        self._options.add_experimental_option(
+            "prefs", prefs
+        )
         if headless:
             self._options.add_argument("--headless")
         if tor:
@@ -31,77 +36,107 @@ class Scraper():
         else :
             self._options.add_argument('user-agent= Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0')
             self._driver = webdriver.Chrome(options=self._options,service=Service(ChromeDriverManager().install()))
-
-
-    def scrapeUrl(self,url,cssSelector=None,attr=None):
+        self._debug = debug
+    '''def scrapeUrl(self,url,cssSelector=None,attr=None):
         if url is None:
             return 
         self._currentUrl = url if url[-1]!= "/" else url[:-1]
-        self._extractLinks(url)
-        self._filterLinks(self._unfilteredLinks)
-        #print(self._extract("title"))
-        self._extract(cssSelector,attr)
+        links = self._filteredLinks(self._extractLinks(url),self._currentUrl)
+        self._extract(cssSelector,attr)'''
 
         
     def _extractLinks(self,url):
         if url is None:
             return
-        self._driver.get(url)
+        if not self._get(url):
+            return 
         content = self._driver.page_source
-        self._soup = BeautifulSoup(content,features="html.parser")
-        self._unfilteredLinks = [ a['href'] for a in self._soup.select("a[href]")]
+        soup = BeautifulSoup(content,features="html.parser")
+        unfilteredLinks = [ a['href'] for a in soup.select("a[href]")]
+        return unfilteredLinks
 
-    def _filterLinks(self,urls):
+    def _filterLinks(self,urls,currentUrl):
         filteredUrls = []
         for url in urls:
             if url.startswith("#"):
                 pass
             elif url.startswith("/") :
-                filteredUrls.append(self._currentUrl + url)
-            elif  url.startswith(self._currentUrl):
+                filteredUrls.append(currentUrl + url)
+            elif  url.startswith(currentUrl):
                 filteredUrls.append(url)
-        self._urls = self._urls + filteredUrls
+        return filteredUrls
+    
+    def _get(self,url:str):
+        if url.endswith(self._blockedExt):
+            if self._debug :
+                print("blocked "+url)
+            return False
+        else:
+            self._driver.get(url)
+            return True
 
-    def _extract(self,cssSelector=None,attr=None):
+    def _extract(self,url,cssSelector=None,attr=None):
         if cssSelector == None:
             return 
-        content = self._soup.select(cssSelector)
+        if not self._get(url):
+            return 
+        content = self._driver.page_source
+        soup = BeautifulSoup(content,features="html.parser")
+        content = soup.select(cssSelector)
         if attr != None:
             try:
-                self._extracted.append(self._extracted + [element[attr] for element in content])
+                self._extracted.add(self._extracted + [element[attr] for element in content])
             except:
-                self._extracted.append(self._extracted + content)    
+                self._extracted.add(self._extracted + content)    
         else:
-            self._extracted.append(self._extracted + content)
+            self._extracted = self._extracted | set(content) 
 
     def getExtracted(self):
         return self._extracted
 
-    def _scrape(self,link,depth):
+    def getWebstack(self):
+        return self._webstack
+
+    def _scrape(self,link,depth,cssSelector=None,attr=None):
+        if self._debug:
+            print("scraping :"+link)
+        self._extract(link,cssSelector,attr)
         if depth == 0:
             return 
-        
+        for v in self._filterLinks(self._extractLinks(link),link):
+            vparse = up.urlparse(v)
+            currentparse = up.urlparse(link)
+            if(vparse.hostname != currentparse.hostname):
+                site = vparse[0]+vparse[1]
+                if  self._webstack.count(site) == 0:
+                    self._webstack.append(site)
+            else : 
+                self._scrape(v,depth-1,cssSelector,attr)
+
+    def pingWebsite(self,site):
+        return os.system("ping -c 1 "+site) == 0
     
-    def scrapeWebsite(self,website,depth=1):
+    def scrapeWebsite(self,website,cssSelector,attr=None,depth=1):
+        if website[-1] == "/":
+            website = website[:-1]
+        if not self.pingWebsite(up.urlparse(website).hostname):
+            return 
         self._webstack.append(website)
         while len(self._webstack) != 0:
             next = self._webstack.popleft()
-            self._scrape(next,depth)
-            #da finire
-        
+            if self._debug :
+                print("scraping website: " + next)
+            self._scrape(next,depth,cssSelector,attr)
 
 
 
-    
-    
+
 
 
 if __name__ == "__main__":
-    scraper = Scraper()
-    try:
-        scraper.scrapeUrl("https://vargiweb.it/")
-    except:
-        pass
-    #print(scraper._urls)
+    scraper = Scraper(debug=True)
+    scraper.scrapeWebsite("https://vargiuweb.it","title")
+    
+    print(scraper.getExtracted())
     #print(up.urlparse("https://ciao.vargiweb.it/").hostname)
 
